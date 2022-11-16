9bfc7274cc96188e3fd8d2c6242cdac9f40b3022
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-11-17 00:46:44 +0300

    feat: add section Tab-3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//_TABS_1
 const LINKS_1 = document.querySelectorAll(".tab-1__link");
 const TABS_1 = document.querySelectorAll(".tab-1__item");
 
@@ -22,6 +23,7 @@ LINKS_1.forEach((link) => {
   });
 });
 
+//_TABS_2
 const LINKS_2 = document.querySelectorAll(".tab-2__link");
 const TABS_2 = document.querySelectorAll(".tab-2__item");
 
@@ -38,6 +40,32 @@ LINKS_2.forEach((link) => {
       });
 
       TABS_2.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+
+      currentTab.classList.add("active");
+      link.classList.add("active");
+    }
+  });
+});
+
+//_TABS_3
+const LINKS_3 = document.querySelectorAll(".tab-3__link");
+const TABS_3 = document.querySelectorAll(".tab-3__item");
+
+LINKS_3.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (!link.classList.contains("active")) {
+      let id = link.dataset.tab;
+      let currentTab = document.querySelector(`#${id}`);
+
+      LINKS_3.forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      TABS_3.forEach((tab) => {
         tab.classList.remove("active");
       });
 
