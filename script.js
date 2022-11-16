c30a8a7b825b81a0758f187d4c61df1065dbafc6
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2022-11-16 17:34:47 +0300

    feat: add section Tab-2

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
-const LINKS = document.querySelectorAll(".tab-1__link");
-const TABS = document.querySelectorAll(".tab-1__item");
+const LINKS_1 = document.querySelectorAll(".tab-1__link");
+const TABS_1 = document.querySelectorAll(".tab-1__item");
 
-LINKS.forEach((link) => {
+LINKS_1.forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     if (!link.classList.contains("active")) {
       let id = link.dataset.tab;
       let currentTab = document.querySelector(`#${id}`);
 
-      LINKS.forEach((link) => {
+      LINKS_1.forEach((link) => {
         link.classList.remove("active");
       });
 
-      TABS.forEach((tab) => {
+      TABS_1.forEach((tab) => {
         tab.classList.remove("active");
       });
 
       link.classList.add("active");
       currentTab.classList.add("active");
+    }
+  });
+});
+
+const LINKS_2 = document.querySelectorAll(".tab-2__link");
+const TABS_2 = document.querySelectorAll(".tab-2__item");
+
+LINKS_2.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (!link.classList.contains("active")) {
+      let id = link.dataset.tab;
+      let currentTab = document.querySelector(`#${id}`);
+
+      LINKS_2.forEach((link) => {
+        link.classList.remove("active");
+      });
+
+      TABS_2.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+
+      currentTab.classList.add("active");
+      link.classList.add("active");
     }
   });
 });
